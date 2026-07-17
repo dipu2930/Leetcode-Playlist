41bbba7817984f4fada0832af3c2d04e085b9498
Dipanjan Aich <aichdipanjan29@gmail.com>
2026-07-17 20:13:52 +0530

    Time: 292 ms (21.63%), Space: 69.1 MB (20.29%) - LeetHub

---
+++ 0595-big-countries/0595-big-countries.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    df=pd.DataFrame(world)
+    res=df[(df["area"]>=3000000)|(df["population"]>=25000000)][["name","population","area"]]
+    return res
+    
