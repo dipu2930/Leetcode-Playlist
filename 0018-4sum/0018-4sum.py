e3a4a9710769771f69078eaa989522c3d4a45d29
Dipanjan Aich <aichdipanjan29@gmail.com>
2026-07-23 19:57:11 +0530

    Time: 398 ms (56.61%), Space: 19.4 MB (53.07%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,19 +1,30 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         n=len(nums)
-        result=set()
-         
+        ans=[]
+        nums.sort()
         for i in range(0,n):
-            # my_set=set()
+            if i>0 and nums[i]==nums[i-1]:
+                continue
             for j in range(i+1,n):
-                my_set=set()
-                for k in range(j+1,n):
-                    fourth=target-(nums[i]+nums[j]+nums[k])
-                    if fourth in my_set:
-                        temp=[nums[i],nums[j],nums[k],fourth]
-                        temp.sort()
-                        result.add(tuple(temp))
-                    
-                    my_set.add(nums[k])
-        return [list(ans) for ans in result]
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                k=j+1
+                l=n-1
+                while k<l:
+                    total=nums[i]+nums[j]+nums[k]+nums[l]
+                    if total==target:
+                        ans.append([nums[i],nums[j],nums[k],nums[l]])
+                        k+=1
+                        l-=1
+                        while k<l and nums[k]==nums[k-1]:
+                            k+=1
+                        while l>k and nums[l]==nums[l+1]:
+                            l-=1
+                    elif total<target:
+                        k+=1
+                    else:
+                        l-=1
+        return ans
+
         
